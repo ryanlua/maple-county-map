d94111b16535fcdd29da8727d879d36cf3257018
Ryan Luu <ryan.luu@ryanluu.dev>
2025-11-11 00:13:47 -0800

    Configure rules
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,4 +4,11 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "no-undef": "off",
+    },
+  },
 ]);
