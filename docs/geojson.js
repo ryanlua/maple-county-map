b25cc6d25db9d139dc4e2b350015d456f17ab863
Ryan Luu <ryan.luu@ryanluu.dev>
2025-11-11 11:56:42 -0800

    Use file defined rules
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

--- docs/geojson.js
+++ docs/geojson.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 var gasStations = {
     "type": "FeatureCollection",
     "features": [
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,11 +4,4 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-
-  {
-    rules: {
-      "no-unused-vars": "off",
-      "no-undef": "off",
-    },
-  },
 ]);
